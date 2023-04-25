35b82a0d52094d6eee6063e3007b76632e2341ff
BI TAH DIDIER TAH <didier.tah@mqashgroup.com>
2023-04-25 11:46:37 +0000

    screens

---
+++ screens/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home.js";
+import Portfolio from "./Portfolio.js";
+import Prices from "./Prices.js";
+import Settings from "./Settings.js";
+import Transaction from "./Transaction.js";
+
+export {
+    Home,
+    Portfolio,
+    Prices,
+    Settings,
+    Transaction
+}
